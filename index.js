1a1b1e12cdfc83c7af5744ca113e392414ac8a0b
dzavi84 <milanvidanovic@yahoo.com>
2021-12-02 12:11:11 +0100

    commit-

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send(`
@@ -14,15 +16,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    req.on('data', (data) => {
-        const parsed = data.toString('utf8').split('&');
-        const formData = {};
-        for (let pair of parsed) {
-            const [key, value] = pair.split('=');
-            formData[key] = value;
-        }
-        console.log(formData);
-    });
+    console.log(req.body);
     res.send('Account Created!');
 });
 
